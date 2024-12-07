79d2eff242dd75362c6d009a47f3039c89871cb2
Gabriel190208 <00001125549580sp@al.educacao.sp.gov.br>
2024-12-07 00:39:13 -0300

    Add files via upload

---
+++ Gabriel/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Carros da Disney',
+    'Quem é o protagonista do filme Carros?',
+    'O protagonista do filme Carros é o carro de corrida Lightning McQueen.'
+);
+
+criaCartao(
+    'Carros da Disney',
+    'Qual é o nome da cidade onde Lightning McQueen encontra o Rust-eze?',
+    'A cidade onde Lightning McQueen encontra o Rust-eze é Radiator Springs.'
+);
+
+criaCartao(
+    'Carros da Disney',
+    'Quem é o melhor amigo de Lightning McQueen?',
+    'O melhor amigo de Lightning McQueen é o caminhão Mack.'
+);
+
+criaCartao(
+    'Carros da Disney',
+    'Qual é o nome do treinador de Lightning McQueen?',
+    'O treinador de Lightning McQueen é o carro velho Doc Hudson.'
+);
